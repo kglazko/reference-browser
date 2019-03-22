4de0359ea2b5f42d35a773a767109543dc2d2a57
Kate Glazko <katglazko@gmail.com>
2019-03-22 16:15:36 -0700

    typo..

--- automation/taskcluster/upload_apk_nimbledroid.py
+++ automation/taskcluster/upload_apk_nimbledroid.py
@@ -46,7 +46,7 @@ data = secrets.get('project/mobile/reference-browser/nimbledroid')
 # disable focus webview upload until https://github.com/mozilla-mobile/focus-android/issues/3574 is resolved
 rb_file_x86 = {'apk': open('app/build/outputs/apk/geckoNightlyX86/debug/app-geckoNightly-x86-debug.apk')}
 rb_file_arm = {'apk': open('app/build/outputs/apk/geckoNightlyArm/debug/app-geckoNightly-arm-debug.apk')}
-rb_file_aarch64 = {'apk': open('geckoNightlyAarch64/debug/app-geckoNightly-aarch64-debug.apk')}
+rb_file_aarch64 = {'apk': open('app/build/outputs/apk/geckoNightlyAarch64/debug/app-geckoNightly-aarch64-debug.apk')}
 
 cwd = os.getcwd()
 print cwd
